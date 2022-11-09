decfa959b079de0c88197763d0ccfc9396a054b9
Srikrishnan B <bsk19.05.1998@gmail.com>
2022-11-09 23:42:39 +0530

    image to pdf tested

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -1,2 +1,7 @@
-from img2pdf import *
+import img2pdf
+
+def convert_img_to_pdf(file_path):
+    with open("./resources/test_pdf.pdf","wb") as f:
+        f.write(img2pdf.convert(file_path))
+convert_img_to_pdf("./resources/Tables.png")
 
